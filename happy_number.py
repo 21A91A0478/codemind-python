c95da6b95fa0fc02ec175fc83fa12b7d243fef62
21A91A0478 <99873476+21A91A0478@users.noreply.github.com>
2022-10-09 23:27:58 +0530

    def sod(n):     s=0     r=0     while n>0:         r=n%10         s=s + r*r         n=n//10     return s
    n=int(input())
    result=n
    while (result!=1 and result!=4):
        result=sod(result)
    if result==1:
        print('True')
    else:
        print('False')

--- happy_number.py
+++ happy_number.py
@@ -1,18 +1,17 @@
-def hpy(n):
-    x=n
-    sum1=0
-    while x>0:
-        c=x%10
-        sum1=(sum1+(c*c))
-        x=x//10
-    return sum1
+def sod(n):
+    s=0
+    r=0
+    while n>0:
+        r=n%10
+        s=s + r*r
+        n=n//10
+    return s
 
 n=int(input())
-hpy(n)
-s=n
-while s>9:
-    s=hpy(s)
-if s==1 or s==7:
+result=n
+while (result!=1 and result!=4):
+    result=sod(result)
+if result==1:
     print('True')
 else:
     print('False')
