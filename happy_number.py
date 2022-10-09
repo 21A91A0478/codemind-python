774f6662f072df52a0b8af181c01a58a93888108
21A91A0478 <99873476+21A91A0478@users.noreply.github.com>
2022-10-09 23:34:56 +0530

    def hpy(n):     s=0     while n>0:         r=n%10         s=s+r*r         n=n//10     return s
    n=int(input())
    res=n
    while res>9:
        res=hpy(res)
    if res==1 or res==7:
        print('True')
    else:
        print('False')

--- happy_number.py
+++ happy_number.py
@@ -1,17 +1,16 @@
-def sod(n):
+def hpy(n):
     s=0
-    r=0
     while n>0:
         r=n%10
-        s=s + r*r
+        s=s+r*r
         n=n//10
     return s
 
 n=int(input())
-result=n
-while (result!=1 and result!=4):
-    result=sod(result)
-if result==1:
+res=n
+while res>9:
+    res=hpy(res)
+if res==1 or res==7:
     print('True')
 else:
     print('False')
