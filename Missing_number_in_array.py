cc938c386a06a7d7095a183a195ad63ea4bf5bf8
21A91A0478 <99873476+21A91A0478@users.noreply.github.com>
2023-05-23 22:49:36 +0530

    for i in range(int(input())):     N = int(input())     l = list(map(int,input().split()))     for i in range(1,N+1):         if i not in l:             print(i)

--- Missing_number_in_array.py
+++ Missing_number_in_array.py
@@ -1,6 +1,6 @@
 for i in range(int(input())):
-    n = int(input())
+    N = int(input())
     l = list(map(int,input().split()))
-    for i in range(1,n+1):
+    for i in range(1,N+1):
         if i not in l:
             print(i)
