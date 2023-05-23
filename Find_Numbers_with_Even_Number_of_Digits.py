3da1a6ae7c72d0f6434d8471addd2ec0d76346a3
21A91A0478 <99873476+21A91A0478@users.noreply.github.com>
2023-05-23 20:44:35 +0530

    n = int(input()) l = list(map(int,input().split())) s = list(map(str,l)) c = 0 for i in s:     if len(i) % 2 == 0:         c += 1 print(c)

--- Find_Numbers_with_Even_Number_of_Digits.py
+++ Find_Numbers_with_Even_Number_of_Digits.py
@@ -1,7 +1,8 @@
 n = int(input())
 l = list(map(int,input().split()))
+s = list(map(str,l))
 c = 0
-for i in l:
-    if len(str(i)) % 2 == 0:
+for i in s:
+    if len(i) % 2 == 0:
         c += 1
 print(c)
