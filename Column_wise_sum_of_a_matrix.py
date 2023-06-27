e78b87d3bb7dfe951df18440114896932c62243a
21A91A0478 <99873476+21A91A0478@users.noreply.github.com>
2023-06-27 21:08:03 +0530

    r, c = map(int,input().split()) mat = [] for i in range(r):     inner_list = list(map(int,input().split()))     mat.append(inner_list)
    l = []
    for i in range(c):
        s = 0
        for j in range(r):
            s += mat[j][i]
        l.append(s)
    print(*l)

--- Column_wise_sum_of_a_matrix.py
+++ Column_wise_sum_of_a_matrix.py
@@ -4,15 +4,10 @@ for i in range(r):
     inner_list = list(map(int,input().split()))
     mat.append(inner_list)
 
-m = [[0 for i in range(r)] for j in range(c)]
-for i in range(r):
-    for j in range(c):
-        m[j][i] = mat[i][j]
-
 l = []
 for i in range(c):
     s = 0
     for j in range(r):
-        s += m[i][j]
+        s += mat[j][i]
     l.append(s)
 print(*l)
