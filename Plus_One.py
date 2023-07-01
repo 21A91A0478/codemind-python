f481d77582264d9db9d04389687ae6abeaeed876
21A91A0478 <99873476+21A91A0478@users.noreply.github.com>
2023-07-01 15:18:16 +0530

    n = int(input()) l = list(map(int,input().split())) num = '' for i in l:     num += str(i) num = int(num) + 1 lst = list(str(num)) print(*lst)

--- Plus_One.py
+++ Plus_One.py
@@ -1,11 +1,8 @@
 n = int(input())
 l = list(map(int,input().split()))
-s = list(map(str,l))
-st = ""
-for i in s:
-    st += i
-lst = int(st) + 1
-lis = list(str(lst))
-e = [ eval(i) for i in lis ]
-for j in e:
-    print(j,end=" ")
+num = ''
+for i in l:
+    num += str(i)
+num = int(num) + 1
+lst = list(str(num))
+print(*lst)
