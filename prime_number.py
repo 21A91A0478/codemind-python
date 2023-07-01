cdf37c95ac90a028d608a0051676885f197e603d
21A91A0478 <99873476+21A91A0478@users.noreply.github.com>
2023-07-01 20:09:26 +0530

    def prime(n):     if n < 2:         return 0     c = 0     for i in range(2, int(n**0.5)+1):         if n % i == 0:             c += 1     if c == 0:         return 1     return 0
    n = int(input())
    if prime(n):
        print("prime")
    else:
        print("not a prime")

--- prime_number.py
+++ prime_number.py
@@ -1,7 +1,16 @@
-n=int(input())
-for i in range(2,n):
-    if(n%i==0):
-        print('not a prime')
-        break
+def prime(n):
+    if n < 2:
+        return 0
+    c = 0
+    for i in range(2, int(n**0.5)+1):
+        if n % i == 0:
+            c += 1
+    if c == 0:
+        return 1
+    return 0
+    
+n = int(input())
+if prime(n):
+    print("prime")
 else:
-    print('prime')
+    print("not a prime")
