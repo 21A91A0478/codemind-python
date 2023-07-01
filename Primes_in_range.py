7f690c3b75b4e845c99b36d15606ab334045646d
21A91A0478 <99873476+21A91A0478@users.noreply.github.com>
2023-07-01 20:50:11 +0530

    # count primes in range def prime(n):     if n < 2:         return False     for i in range(2, int(n**0.5)+1):         if n % i == 0:             return False     return True
    n = int(input())
    m = int(input())
    s = 0
    for i in range(n, m+1):
        if prime(i):
            s += 1
    print(s)

--- Primes_in_range.py
+++ Primes_in_range.py
@@ -1,15 +1,16 @@
-def is_prime(n):
-    if n==1:
+# count primes in range
+def prime(n):
+    if n < 2:
         return False
-    s = int(n**0.5)
-    for i in range(2,s+1):
-        if n%i == 0:
+    for i in range(2, int(n**0.5)+1):
+        if n % i == 0:
             return False
     return True
+
 n = int(input())
 m = int(input())
-c = 0
-for j in range(n,m+1):
-    if is_prime(j):
-        c+=1
-print(c)
+s = 0
+for i in range(n, m+1):
+    if prime(i):
+        s += 1
+print(s)
