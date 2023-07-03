c94d7b40aac1d2c213ed68bfca1d9ac66688b6f6
21A91A0478 <99873476+21A91A0478@users.noreply.github.com>
2023-07-03 12:13:50 +0530

    s1 = input().lower() s2 = input().lower() l1 = [] l2 = [] for i in s1:     if i not in l1 and i.isalpha():         l1.append(i) for i in s2:     if i not in l2 and i.isalpha():         l2.append(i) l = [] for i in l1:     if i in l2:         l.append(i) l.sort() s = '' for i in l:     s += i if s:     print(s) else:     print(-1)

---
+++ common_characters.py
@@ -0,0 +1,22 @@
+s1 = input().lower()
+s2 = input().lower()
+l1 = []
+l2 = []
+for i in s1:
+    if i not in l1 and i.isalpha():
+        l1.append(i)
+for i in s2:
+    if i not in l2 and i.isalpha():
+        l2.append(i)
+l = []
+for i in l1:
+    if i in l2:
+        l.append(i)
+l.sort()
+s = ''
+for i in l:
+    s += i
+if s:
+    print(s)
+else:
+    print(-1)
