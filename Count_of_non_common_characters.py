04d973100426cce1c302e50dd9fb27c0abb565ed
21A91A0478 <99873476+21A91A0478@users.noreply.github.com>
2023-07-03 12:17:14 +0530

    s1 = input().lower() s2 = input().lower() l1 = [] l2 = [] for i in s1:     if i not in l1 and i.isalpha():         l1.append(i) for i in s2:     if i not in l2 and i.isalpha():         l2.append(i) l = [] for i in l1:     if i not in l2:         l.append(i) for i in l2:     if i not in l1:         l.append(i) print(len(l))

---
+++ Count_of_non_common_characters.py
@@ -0,0 +1,18 @@
+s1 = input().lower()
+s2 = input().lower()
+l1 = []
+l2 = []
+for i in s1:
+    if i not in l1 and i.isalpha():
+        l1.append(i)
+for i in s2:
+    if i not in l2 and i.isalpha():
+        l2.append(i)
+l = []
+for i in l1:
+    if i not in l2:
+        l.append(i)
+for i in l2:
+    if i not in l1:
+        l.append(i)
+print(len(l))
