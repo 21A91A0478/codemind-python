fa58ad76610b8fc0b0ce61c798c6bc35a0289a6d
21A91A0478 <99873476+21A91A0478@users.noreply.github.com>
2023-07-13 23:39:09 +0530

    n = int(input()) l = list(map(int,input().split())) s = '' for i in l:     s += str(i) r = s[::-1] a = [] for i in range(len(r)):     if r[i] == '1':         a.append(2 ** i) print(sum(a))

--- Binary_Array_to_decimal.py
+++ Binary_Array_to_decimal.py
@@ -3,5 +3,9 @@ l = list(map(int,input().split()))
 s = ''
 for i in l:
     s += str(i)
-r = int(s, 2)
-print(r)
+r = s[::-1]
+a = []
+for i in range(len(r)):
+    if r[i] == '1':
+        a.append(2 ** i)
+print(sum(a))
